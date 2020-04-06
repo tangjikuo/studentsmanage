2de28ff37dad93144a33c201edcb6f8ccf607633
tangjikuo <351480441@qq.com>
2020-04-06 14:50:35 +0800

    create database tables

---
+++ SQL/insert_student.sql
@@ -0,0 +1,3 @@
+INSERT INTO studentsmanage.student VALUES (
+    '汤纪阔', '男', '351480441', '123', '100'
+                                         );
---
+++ SQL/insert_teacher.sql
@@ -0,0 +1,3 @@
+INSERT INTO studentsmanage.teacher VALUES (
+       '余晓慧','女','123','520'
+                                          );
